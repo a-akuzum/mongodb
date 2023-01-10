337dde3250fea883e3669d210c57960d28dc629a
= <a.akuzum@gmail.com>
2023-01-09 20:35:42 -0800

    finished

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-//connection addresss here 
+//mongo hostname to connect here
 const { Schema } = mongoose;
 
 //..........user schema
@@ -14,14 +14,57 @@ const userSchema = new Schema({
 
 const User = mongoose.model('User', userSchema)
 async function fetchUser(){
-    // const users = await User.find({ isMarried: true , salary: 0});
-    const users = await User.findById("63bcd179852d29c593343b9e");
+    // const users = await User.findById("63bcd179852d29c593343b9e");
+
+    // QUERY SELECT
+    // const users = await User.find({ isMarried: true })
+    // .select('name salary')
+    // .sort('-salary')
+    // .limit(1)
+
+    //COMPARISON OPERATORS
+    // const users = await User.find({ age: { $gte: 30} }) //gt, gte, lt, lte, in, nin
+    // const users = await User.find({ salary: { $in: [80000, 70000]} }) //gt, gte, lt, lte, in, nin
+    
+    //AND OR OPERATORS
+    // const users = await User.find({ salary: { $in: [80000, 70000]} })
+    //                 .and([{ isMarried: true }, { age: { $lt: 40}} ])  //AND
+                    
+    const users = await User.find({ age: {$gt: 30}})
+                            .or({ isMarried: false})
+                            .select('name')
+                            .sort('name')
+
     console.log(users)
 }
 
-fetchUser();
+// fetchUser();
 
+//UPDATING A DB
+async function updateUserInfo1(){
+    const user = await User.findById("63bcd179852d29c593343b9b");
+    user.salary = 1000000;
+    await user.save()
+    console.log(user)
+}
+async function updateUserInfo2(){
+    const user = await User.findByIdAndUpdate("63bcd179852d29c593343b9b", 
+    {age: 37, salary: 20000000}, {new: true, runValidators: true})
+    
+    console.log(user)
+}
 
+updateUserInfo1();
+
+//DELETING USER FROM DB
+async function deleteUser() {
+  const user = await User.findByIdAndDelete("63bcd179852d29c593343b9b",
+  );
+
+  console.log(user);
+}
+
+deleteUser();
 // async function storeUserInfo(){
 //     const user = new User({
 //       name: "Aaron",
